e732812fc9a56dee1b25804d5c1a04eeb2e72749
Michael Mouratidis <mfk_poison@Hotmail.com>
2017-11-03 14:17:45 +0200

    Test

--- src/main/java/com/weatherapi/weatherapi/WeatherApiApplication.java
+++ src/main/java/com/weatherapi/weatherapi/WeatherApiApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeatherapiApplication {
+public class WeatherApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WeatherapiApplication.class, args);
+		SpringApplication.run(WeatherApiApplication.class, args);
 	}
 }
